b4ac04d6c19a4213447b51a30084aac4b17ee1e2
shalevo <omrimexico@gmail.com>
2019-02-02 11:33:52 +0200

    main starter

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
+typedef struct treeNode {
+    char *word;
+    unsigned short wordId;
+    struct treeNode *left;
+    struct treeNode *right;
+} TreeNode;
+
+typedef struct tree {
+    TreeNode *root;
+} WordTree;
+
+WordTree buildTree(char * fileName);
+void updateTree(WordTree *wt, char * word);
+TreeNode * createNode (char  * word);
+
+
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    FILE * f = fopen("./reuters_train.txt","r");
+    buildTree(f);
+
+}
+
+WordTree buildTree(char * fileName) {
+
 }
