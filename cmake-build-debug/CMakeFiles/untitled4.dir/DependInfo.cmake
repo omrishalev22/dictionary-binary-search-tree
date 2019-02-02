f89250ae5b65e99a0cfddc0543d21b2c5c062a7f
shalevo <omrimexico@gmail.com>
2019-02-02 11:33:15 +0200

    main starter

--- cmake-build-debug/CMakeFiles/untitled4.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/untitled4.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/shalevo/CLionProjects/untitled4/main.c" "/home/shalevo/CLionProjects/untitled4/cmake-build-debug/CMakeFiles/untitled4.dir/main.c.o"
+  "/home/shalevo/CLionProjects/dictionary-binary-search-tree/main.c" "/home/shalevo/CLionProjects/dictionary-binary-search-tree/cmake-build-debug/CMakeFiles/untitled4.dir/main.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
